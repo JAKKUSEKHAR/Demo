7c6f983e911a7d33867e20aa25e892f6ac503fcc
JAKKU SEKHAR <jakkusekhar22@gmail.com>
2025-11-08 08:05:30 +0530

    Second Commit

--- src/com/demo/Demo.java
+++ src/com/demo/Demo.java
@@ -2,6 +2,6 @@ package com.demo;
 
 public class Demo {
 public static void main(String[] args) {
-	System.out.println("Hello Java good morning");
+	System.out.println("Hello Java  good morning");
 }
 }
